fb8b0473499f8518046f12f3961e6db622974ed7
arnavgosain <arnav.gosain@gmail.com>
2025-08-31 17:51:47 -0700

    moved google client id to constants

--- constants.js
+++ constants.js
@@ -1,2 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL
 export const MAPBOX_ACCESS_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
