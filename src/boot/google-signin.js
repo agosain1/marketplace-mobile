9f63eb53347ce37b582fac2cf02a0b0b95584d05
arnavgosain <arnav.gosain@gmail.com>
2025-09-03 21:57:42 -0700

    cleaned up code

--- src/boot/google-signin.js
+++ src/boot/google-signin.js
@@ -1,5 +1,13 @@
 import { defineBoot } from '#q-app/wrappers'
-import { googleAuthConfig } from '../config/google.js'
+
+ import { GOOGLE_CLIENT_ID } from '../../constants.js'
+
+export const googleAuthConfig = {
+  clientId: GOOGLE_CLIENT_ID,
+  scope: 'openid email profile',
+  prompt: 'select_account'
+}
+
 
 
 export default defineBoot(async ({ app }) => {
