ec126bf0ff73c0f03b37c15ac063fc6b7595e8da
arnavgosain <arnav.gosain@gmail.com>
2025-09-03 19:47:58 -0700

    attemping to fix google issue

--- src/boot/google-signin.js
+++ src/boot/google-signin.js
@@ -1,12 +1,5 @@
 import { defineBoot } from '#q-app/wrappers'
-
-import { GOOGLE_CLIENT_ID } from '../../constants.js'
-
-export const googleAuthConfig = {
-  clientId: GOOGLE_CLIENT_ID,
-  scope: 'openid email profile',
-  prompt: 'select_account'
-}
+import { googleAuthConfig } from '../config/google.js'
 
 
 export default defineBoot(async ({ app }) => {
