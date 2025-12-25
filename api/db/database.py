47d1c71ad6b74f1393f9f18381b79395096997fc
agosain1 <arnav.gosain@gmail.com>
2025-12-25 00:35:47 -0800

    remove unnecessary import psycopg

--- api/db/database.py
+++ api/db/database.py
@@ -1,7 +1,5 @@
 from dotenv import load_dotenv
 import os
-import psycopg
-from psycopg.rows import dict_row
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -17,17 +15,6 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def get_db_connection():
-    """Returns a database connection"""
-    return psycopg.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        row_factory=dict_row
-    )
 
 def get_db():
     """Dependency function for SQLAlchemy sessions"""
