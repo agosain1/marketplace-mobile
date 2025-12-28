3616fea23426893f7a7c1bd2b532c8ffcc350064
agosain1 <arnav.gosain@gmail.com>
2025-12-28 14:18:24 -0800

    pyscopg fix: remove explicit driver specification in db

--- api/db/database.py
+++ api/db/database.py
@@ -12,7 +12,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # SQLAlchemy setup - explicitly use psycopg driver
-DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
